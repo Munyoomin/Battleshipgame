aede81924bdc63e29c6b1ee237589e47aa76374f
Munyoomin <joshuamun31@gmail.com>
2018-09-26 16:39:52 +0800

    First commit

---
+++ Model/ShipName.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace battleshipsgame
+{
+public enum ShipName
+{
+
+	None = 0,
+
+	Tug = 1,
+
+	Submarine = 2,
+
+	Destroyer = 3,
+
+	Battleship = 4,
+
+	AircraftCarrier = 5,
+}
+}
